2417997e2f94ebec7b0243ee82605b3b6e4562ae
naikshailesh11 <naikshailu@yahoo.com>
2017-03-05 13:07:57 +0530

    First Commit

---
+++ MVCPractise/MVCPractise/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractise.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GoToHome()
+        {
+            return View("MyHomePage");
+        }
+    }
+}
