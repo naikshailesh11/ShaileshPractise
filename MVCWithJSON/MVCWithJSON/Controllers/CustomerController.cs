5fa9ee8612576ee93afc0aa40bdae7dddbb377e2
naikshailesh11 <naikshailu@yahoo.com>
2017-03-11 19:48:58 +0530

    Json practise

---
+++ MVCWithJSON/MVCWithJSON/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWithJSON.Models;
+namespace MVCWithJSON.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getCustomer()
+        {
+            Customer obj = new Customer();
+            obj.CustomerCode = "1001";
+            obj.CustomerName = "Shailesh";
+            return Json(obj,JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult DisplayCustomer()
+        {
+            getCustomer();
+            return View("ConsumeCustomer");
+        }
+    }
+}
---
+++ MVCWithJSON/MVCWithJSON/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWithJSON.MvcApplication" Language="C#" %>
