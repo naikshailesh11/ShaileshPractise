b0ed25237ad1b4d01d9d190813553606c00d54ed
naikshailesh11 <naikshailu@yahoo.com>
2017-03-26 21:25:25 +0530

    QuestPondLab6

---
+++ CustomerServer/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CustomerServer.Models;
+namespace CustomerServer.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        // GET: api/Customer
+        List<Customer> cust=new List<Customer>();
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Customer/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customer
+        public List<Customer> Post(Customer obj)
+        {
+         
+            cust.Add(obj);
+            return cust;
+        }
+
+        // PUT: api/Customer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Customer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CustomerServer/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerServer.Models
+{
+    public class Customer
+    {
+        public  string CustomerName { get; set; }
+        public string CustomerCode { get; set; }
+        public string CustomerAmount { get; set; }
+        public string SalesDateTime { get; set; }
+    }
+}
